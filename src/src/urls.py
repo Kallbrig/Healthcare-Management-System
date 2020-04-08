fbe4b2db4b985069e5039461a3cb77990e85eff7
Kristopher Allbright <kallbright@troy.edu>
2020-04-08 16:17:33 -0500

    Added Accounts, Appointments, Records, and Reports apps

--- src/src/urls.py
+++ src/src/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('reports/', include('reports.urls')),
+    path('appointments/', include('appointments.urls')),
+    path('records/', include('records.urls')),
+
 ]
