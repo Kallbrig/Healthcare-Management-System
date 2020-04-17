d45dbd0f6e228cc5d547217266fa2cf754a624a9
Kristopher Allbright <kallbright@troy.edu>
2020-04-17 17:10:29 -0500

    Added login as default landing page.

--- src/src/urls.py
+++ src/src/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts import views as account_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('records/', include('records.urls'))
+    path('records/', include('records.urls')),
+    path('', account_views.login,  ),
 
 
 ]
