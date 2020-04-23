bdfe20833065136f65392b3f5069815638eaa295
Garrett <gdavies@troy.edu>
2020-04-23 13:09:10 -0500

    Added all changes from accounts branch

--- src/src/settings.py
+++ src/src/settings.py
@@ -57,7 +57,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'templates/'
+            'templates/',
+            'src/templates/'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
