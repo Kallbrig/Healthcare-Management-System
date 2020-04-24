dad8fd88fc7c07f89492352a392b3ed9084428c6
Garrett <gdavies@troy.edu>
2020-04-23 21:59:51 -0500

    Fixed src location for templates

--- src/src/settings.py
+++ src/src/settings.py
@@ -59,6 +59,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             'templates/',
+            'src/templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
